f03cf8c731603e567ebe898fa51c21f0c841eb4b
DonHimanshu2003 <77272555+DonHimanshu2003@users.noreply.github.com>
2022-12-26 20:41:52 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,258 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+
+int main()
+{/*
+	// prints hello world
+	cout << "Hello World";
+
+	int a;
+	cin>>a;
+	if (a>0)
+		cout<<a<<endl;
+	else
+		cout<<-(a)<<endl;
+
+	cout<<"namste dunia :-)"<<endl;
+
+	int q=123;
+	cout<<q<<endl;
+	char b='v';
+	cout<<b<<endl;
+	bool b1=true;
+	cout<<b1<<endl;
+	bool b2=false;
+	cout<<b2<<endl;
+	float c=1.2;
+	cout<<c<<endl;
+	double d=1.234;
+	cout<<d<<endl;
+	int size=sizeof(d);
+	cout<<size<<endl; 
+	
+	int a='a';
+	cout<<a<<endl;
+	char ch=98;
+	cout<<ch<<endl;
+	char ch1=123456;
+	cout<<ch1<<endl;
+	unsigned int f=132;
+	cout<<f<<endl;
+	
+	unsigned int f=-123;
+	cout<<f<<endl;
+
+	int a=2/5;
+	cout<<a<<endl;
+
+	float a1=2.0/5;
+	cout<<a1<<endl;
+	
+	int a=3;
+	int b=4;
+	bool first=(a==b);
+	cout<<first<<endl;
+
+	bool second=(a>b);
+	cout<<second<<endl;
+	
+	bool third=(a<b);
+	cout<<third<<endl;
+	
+	bool fourth=(a>=b);
+	cout<<fourth<<endl;
+	
+	bool fifth=(a<=b);
+	cout<<fifth<<endl;
+	
+	bool sixth=(a!=b);
+	cout<<sixth<<endl;
+	
+	cout<<!a<<endl;
+
+	int a=2;
+	cin>>a;
+	if(a>0)
+		cout<<"positive"<<endl;
+	else
+		cout<<"negative"<<endl;	
+		
+	int a,b;
+	cin>>a>>b;
+	if(a>b)
+		cout<<"A is greater then b"<<endl;
+	else if(b>a)	
+		cout<<"B is greater then a"<<endl;
+	
+	int a=2;
+	int b=a+1;
+	if((a=3)==b)
+		cout<<a;
+	else
+		cout<<a+1;
+
+	int upper = 0, lower = 0, number = 0, special = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            upper++;
+        else if (str[i] >= 'a' && str[i] <= 'z')
+            lower++;
+        else if (str[i]>= '0' && str[i]<= '9')
+            number++;
+        else
+            special++;
+    }
+	string str = "#GeeKs01fOr@gEEks07";
+    Count(str);
+	
+	int n;
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		cout<<i<<" ";
+		i=i+1;	
+	}
+
+	int n=0;
+	cin>>n;
+	int sum=0;
+	int i=1;
+	while (i<=n){
+		sum=sum+i;
+		i=i+1;
+	}
+	cout<<sum;
+
+	int n=0;
+	cin>>n;
+	int sum=0;
+	int i=2;
+	while(i<=n){
+		sum=sum+i;
+		i=i+2;
+	}	
+	cout<<sum;
+	
+	int frahent;
+	cin>>frahent;
+	float celsius=((frahent-32)*5)/9;
+	cout<<celsius<<endl;
+
+	int n;
+	cin>>n;
+	int i=2;
+	while (i<n)
+	{
+
+		if(n%i==0){
+			cout<<"not prime"<<endl;
+		}
+		else{
+			cout<<"prime"<<endl;
+		}
+		i=i+1;
+	}		
+	
+	int n;
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+			cout<<" * ";
+			j=j+1;
+
+		}
+		cout<<endl;
+		i=i+1;
+	}
+	
+	int n;
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+			cout<<" "<<i<<" ";
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	}
+	
+	int n;
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+			cout<<" "<<j<<" ";
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	}
+	
+	int n;
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+			cout<<" "<<n-j+1<<" ";
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	}
+	
+	int n;
+	cin>>n;
+	int i=1;
+	int count=1;
+
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+			cout<<count<<" ";
+			count=count+1;
+			j=j+1;
+		}
+	cout<<endl;
+	i=i+1;
+	}
+	
+	int n;
+	cin>>n; 
+	int row=1;
+	while(row<=n){
+		int col=1;
+		while(col<=row){
+			cout<<" * ";
+			col=col+1;
+		}
+		cout<<endl;
+		row=row+1;
+	}
+	
+	int n;
+	cin>>n;
+	int row=1;
+	int count=1;
+	while(row<=n){
+		int col=1;
+		while(col<=row){
+			cout<<" "<<count<<" ";
+			count=count+1;
+			col=col+1;
+		}
+	cout<<endl;
+	row=row+1;
+	}
+	*/
+	return 0;		
+}
